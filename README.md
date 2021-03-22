6e03b3833ba0dbf5aca21939e64cf1d93af4e9c0
rain <rain@pop-os.localdomain>
2021-03-22 20:06:45 +0700

    initial works

---
+++ README.md
@@ -0,0 +1,14 @@
+# ALKAL-SECURE-API
+
+> Secure api for alkal backend
+
+**How to build from github repository:**
+
+using ssh: 
+```
+git clone git@github.com:steamyrain/alkal-secure-api
+cd alkal-secure-api
+php spark migrate
+php spark db:seed ClientSeeder
+php spark serve
+```
---
+++ app/Database/Seeds/ClientSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+use Faker\Factory;
+
+class ClientSeeder extends Seeder
+{
+	public function run()
+	{
+        for($i=0;$i<10;$i++){
+            $this->db->table('client')->insert($this->generateClient());
+        }
+	}
+
+    private function generateClient(): array 
+    {
+        $faker = Factory::create();
+        return [
+            'name'=>$faker->name(),
+            'email'=>$faker->email
+        ];
+    }
+}
